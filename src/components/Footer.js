7555629e0574bd2e4616d586e998a81eeae4ca2d
Tristan Mclennan <Tmclennan@mail.sfsu.edu>
2021-02-04 16:30:46 -0800

    fixed class issue with blog container, footer content, margin for navbar logo.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ import React from "react"
 import SocialLinks from "../constants/socialLinks"
 const Footer = () => {
   return (<footer className="footer">
-      <>
+      <div>
         <SocialLinks styleClass="footer-links" />
         <h4>copyright&copy;{new Date().getFullYear()}
           <span>TmDesign</span> all rights reserved
         </h4>
-      </>
+      </div>
     </footer>)
 }
 
